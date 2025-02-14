56b46b416d1ecc87e4c9e10df4de985a15d7e3ff
alscksdlek <mseon916@naver.com>
2025-02-14 11:51:01 +0900

    feat: sliding session

--- src/auth/guard/access-token.guard.ts
+++ src/auth/guard/access-token.guard.ts
@@ -1,17 +1,20 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { AsyncLocalStorage } from 'async_hooks';
+import { Response } from 'express';
 import { IAsyncLocalStorage } from '#common/als/als.type.js';
+import { TOKEN_EXPIRATION } from '#configs/jwt.config.js';
 
 @Injectable()
 export class AccessTokenGuard implements CanActivate {
   constructor(
-    private jwtService: JwtService,
-    private als: AsyncLocalStorage<IAsyncLocalStorage>,
+    private readonly jwtService: JwtService,
+    private readonly als: AsyncLocalStorage<IAsyncLocalStorage>,
   ) {}
 
   canActivate(context: ExecutionContext): boolean {
     const request = context.switchToHttp().getRequest();
+    const response = context.switchToHttp().getResponse<Response>();
     const token = request.headers['authorization']?.split(' ')[1];
 
     if (token) {
@@ -26,6 +29,12 @@ export class AccessTokenGuard implements CanActivate {
           store.userId = decoded.userId;
           store.userRole = decoded.role;
         }
+
+        // // 새로운 Access Token 발급
+        const payload = { userId: decoded.userId, role: decoded.role };
+        const options = { expiresIn: TOKEN_EXPIRATION.ACCESS };
+        const newAccessToken = this.jwtService.sign(payload, options);
+        response.setHeader('new-access-token', newAccessToken);
       } catch (e) {
         request.user = null;
       }
